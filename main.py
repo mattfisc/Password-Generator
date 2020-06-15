9ad96f4f33ce5813c580b948ca614d8919ceea57
Matthew Fischer <mattfisc@gmail.com>
2020-06-15 03:58:29 -0700

    api optional -error
    optional method not working

--- main.py
+++ main.py
@@ -37,3 +37,32 @@ async def read_item(pwd_length: int, use_symbols: bool):
     from PasswordGenerator import pwdg as file
     pwd = file.PasswordGenerator().generate_password(p_length=pwd_length, use_symbols=use_symbols)
     return {"password": pwd}
+
+
+#optional parameters
+@app.get("/pass/{p_length}{use_symbol}")
+async def read_item(p_length: int = None, use_symbols: bool = None):
+
+    from PasswordGenerator import pwdg as file
+
+    if p_length:
+        pwd = file.PasswordGenerator().generate_password(p_length=p_length,use_symbols=use_symbols)
+    else:
+        pwd = file.PasswordGenerator().generate_password()
+
+    #test
+    print(pwd)
+    return {"password": pwd}
+
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(
+#     user_id: int, item_id: str, q: str = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
